3b8b927a4e2afbf47b439c354bc2312d7cf9b015
samudraiqbal <muhamad_samudra_24rpl@student.smktelkom-mlg.sch.id>
2017-10-29 20:01:36 +0700

    Add Marker

--- app/src/main/java/id/sch/smktelkom_mlg/learn/samudraiqbal/googlemaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/samudraiqbal/googlemaps/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -42,11 +43,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             .build();
     GoogleMap m_map;
     boolean mapReady = false;
+    MarkerOptions Indonesia, France, UnitedState, Australia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Indonesia = new MarkerOptions()
+                .position(new LatLng(-6.175392, 106.827178))
+                .title("Monumen Nasional");
+
+        France = new MarkerOptions()
+                .position(new LatLng(48.858270, 2.294509))
+                .title("Eiffel Tower");
+
+        UnitedState = new MarkerOptions()
+                .position(new LatLng(38.897678, -77.036477))
+                .title("The White House");
+
+        Australia = new MarkerOptions()
+                .position(new LatLng(-33.856820, 151.215279))
+                .title("Sydney Opera House");
 
         Button btnAustralia = (Button) findViewById(R.id.btnAustralia);
         btnAustralia.setOnClickListener(new View.OnClickListener() {
@@ -88,6 +106,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapReady = true;
         m_map = map;
         m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        m_map.addMarker(Indonesia);
+        m_map.addMarker(France);
+        m_map.addMarker(UnitedState);
+        m_map.addMarker(Australia);
         flyTo(INDONESIA);
     }
 
